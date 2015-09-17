6d1d29b8c02bc837314a10c518f07118dd1d5831
bronwen-cassidy <bronwen.cassidy@gmail.com>
2015-09-17 11:57:52 +0100

    Lots of clean up, when this is out we create a version

--- talent-studio-application/db/wf/qacleanup.sql
+++
@@ -1,5 +0,0 @@
----------------------------------------
--- remove any notification left behind
----------------------------------------
-DELETE from WF_NOTIFICATIONS WHERE CONTEXT LIKE('QUESTION%') AND (STATUS='CLOSED' or STATUS='OPEN');
-commit;
